bde5935071741dda3f138592016ef51ba9c3b6ce
Ryoya Maezawa <maezawa@obic.co.jp>
2020-03-04 10:54:37 +0900

    First Commit

---
+++ src/AtCoder/AtCoder/StringUnionFind.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtCoder
+{
+    class StringUnionFind
+    {
+        private Dictionary<string, string> _parent = new Dictionary<string, string>();
+        private Dictionary<string, int> _size = new Dictionary<string, int>();
+    }
+}
