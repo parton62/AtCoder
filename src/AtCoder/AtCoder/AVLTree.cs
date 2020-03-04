665021372b6f91d8b88994aef9d9753f473b8b24
Ryoya Maezawa <maezawa@obic.co.jp>
2020-03-04 18:33:06 +0900

    仮追加

---
+++ src/AtCoder/AtCoder/AVLTree.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtCoder
+{
+    class AVLTree<T>
+    {
+
+
+        class Node<T>
+        {
+            private AVLTree<T> _tree;
+
+            public Node<T> Left { get; set; }
+            public Node<T> Right { get; set; }
+            public Node<T> Parent { get; set; }
+
+            private int Bias()
+            {
+                return 0;
+            }
+
+
+
+        }
+
+    }
+}
