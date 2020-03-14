952dd522f0b15abf6301c44d77a8e94865ea7475
parton <mobileparton@gmail.com>
2020-03-15 00:35:14 +0900

    削除

--- src/AtCoder/AtCoder/Program.cs
+++ src/AtCoder/AtCoder/Program.cs
@@ -8,7 +8,7 @@ namespace AtCoder
     {
         public static void Main(string[] args)
         {
-            Execute(typeof(Contests.ABC157.C));
+            ExecuteLive(typeof(Contests.ABC157.C));
         }
 
         static void ExecuteLive(Type probrem, int? index=null)
@@ -24,52 +24,6 @@ namespace AtCoder
             var e = new DebugExecutor();
             e.Initialize(probrem);
             e.Execute();
-        }
-
-
-
-        static void Sample()
-        {
-            var dic = new Dictionary<string, string>();
-            
-            var a = new List<(string Source, string Dest)>()
-            {
-                ("a","b"),
-                ("a","b"),
-                ("a","b"),
-                ("b","c"),
-                ("c","d"),
-                ("e","d"),
-                ("f","g"),
-                ("h","g"),
-                ("i","j"),
-            };
-
-            Func<string, string> fd = null;
-            fd = (string x) =>
-            {
-                return dic.TryGetValue(x, out var f) ? dic[x] = fd(f) : x;
-            };
-
-            foreach (var item in a)
-            {
-                var s = item.Source;
-                var d = item.Dest;
-
-                dic[s] = fd(d);
-            }
-
-
-            foreach (var item in a)
-            {
-                Console.WriteLine($"依存元:{item.Source}, 　　依存先:{item.Dest}");
-            }
-            Console.WriteLine();
-
-            foreach (var item in a)
-            {
-                Console.WriteLine($"依存元:{item.Source}, 最終依存先:{fd(item.Source)}");
-            }
         }
     }
 }
