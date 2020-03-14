e311f9bece7d81329c661186e1729816c30630db
parton <mobileparton@gmail.com>
2020-03-14 22:36:37 +0900

    整理

---
+++ src/AtCoder/AtCoder/Contests/ABC158/B.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtCoder.Contests.ABC158
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    static class B
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine();
+            
+            if (s[0] == s[1] && s[0] == s[2]) Console.WriteLine("No");
+            else Console.WriteLine("Yes");
+        }
+    }
+}
--- src/AtCoder/AtCoder/Library/AVLTree.cs
+++ src/AtCoder/AtCoder/Library/AVLTree.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace AtCoder
+namespace AtCoder.Library
 {
     class AVLTree<T>
     {
