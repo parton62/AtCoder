8944b418bdb4268932cd27dfa01bfa8ca94c0c9a
parton <mobileparton@gmail.com>
2020-03-20 22:46:28 +0900

    Z

--- src/AtCoder/CopyCoder/CopyCoder.Test/CopyCoderUnitTests.cs
+++ src/AtCoder/CopyCoder/CopyCoder.Test/CopyCoderUnitTests.cs
@@ -18,6 +18,7 @@ namespace CopyCoder.Test
         [DataRow(クラス参照途中)]
         [DataRow(変数参照)]
         [DataRow(いきなりa)]
+        [DataRow(ZAlgorithm)]
         public void 何もせずスルー(string test)
         {
             VerifyCSharpDiagnostic(test);
@@ -38,7 +39,6 @@ namespace CopyCoder.Test
         }
         [DataTestMethod]
         [DataRow(別名前空間Hoge参照, 別名前空間Hoge参照fix)]
-        [DataRow(名前空間なし, 名前空間なしfix)]
         public void 置き換え(string source, string edited)
         {
             VerifyCSharpFix(source, edited, allowNewCompilerDiagnostics:true);
@@ -147,6 +147,54 @@ namespace ConsoleApplication1
 namespace ConsoleApplication2
 {
     class Hoge { }
+}
+";
+
+        private const string ZAlgorithm = @"using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtCoder.Library
+{
+    static class ZAlgorithm
+    {
+        /// <summary>
+        /// s と その各部分文字列(s[i:|s|-1] との最長共通接頭辞の長さ をもつ配列a を構築
+        /// a[0]==|s|
+        /// </summary>
+        /// <param name=""s""></param>
+        public static int[] Solve(string s)
+    {
+        var len = s.Length;
+
+        var a = new int[len];
+        a[0] = len;
+        var i = 1;
+        var j = 0;
+        while (i < len)
+        {
+            while (i + j < len && s[j] == s[i + j]) j++;
+            a[i] = j;
+
+            if (j == 0)
+            {
+                i++;
+                continue;
+            }
+
+            var k = 1;
+            while (i + k < len && k + a[k] < j)
+            {
+                a[i + k] = a[k];
+                k++;
+            }
+            i += k;
+            j -= k;
+        }
+
+        return a;
+    }
+}
 }
 ";
     }
