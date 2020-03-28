7fe47a9f80576684e22dfa88901084679396a5d0
parton <mobileparton@gmail.com>
2020-03-29 00:18:48 +0900

    delete unused regerences

--- src/AtCoder/AtCoder/Executors/LiveTestCaseProvider.cs
+++ src/AtCoder/AtCoder/Executors/LiveTestCaseProvider.cs
@@ -1,18 +1,12 @@
-﻿using AngleSharp;
-using AngleSharp.Html.Dom;
-using AngleSharp.Html.Parser;
-using Microsoft.Data.Sqlite;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net.Http;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace AtCoder.Executors
 {
