a00ac51ac12b151893d861a9feb2f350f46c249a
parton <mobileparton@gmail.com>
2020-04-28 14:43:19 +0900

    add

---
+++ src/AtCoder/AtCoder/Library/MathEx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtCoder.Library
+{
+    class MathEx
+    {
+        public static bool chmax<T>(ref T x, T y) where T : IComparable<T>
+        {
+            if (x.CompareTo(y) < 0)
+            {
+                x = y;
+                return true;
+            }
+            return false;
+        }
+        public static bool chmin<T>(ref T x, T y) where T : IComparable<T>
+        {
+            if(x.CompareTo(y) > 0)
+            {
+                x = y;
+                return true;
+            }
+            return false;
+        }
+    }
+}
