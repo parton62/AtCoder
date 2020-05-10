a57934a41010ab7307a210bc8511a4bffdaa2a67
parton <mobileparton@gmail.com>
2020-05-10 20:59:24 +0900

    156

--- src/AtCoder/AtCoder/Program.cs
+++ src/AtCoder/AtCoder/Program.cs
@@ -9,7 +9,7 @@ namespace AtCoder
     {
         public static void Main(string[] args)
         {
-            ExecuteLive(typeof(Contests.ABC164.D));
+            ExecuteLive(typeof(Contests.ABC156.D));
         }
 
         static void ExecuteLive(Type probrem, int? index=null)
